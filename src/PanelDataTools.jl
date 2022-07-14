6870d3e615155771c8177e98a47666e3b8798a42
Eirik Eylands Brandsaas <eebrandsaas@gmail.com>
2022-07-13 22:23:31 -0400

    Initial functoins

--- src/PanelDataTools.jl
+++ src/PanelDataTools.jl
@@ -1,5 +1,38 @@
 module PanelDataTools
 
 # Write your package code here.
+using DataFrames
+
+function spell!(df,PID::Symbol,TID::Symbol,var::Symbol)
+    for var in ["_end", "_seq", "_spell"]
+        if "$var" ∈ Set(names(df))
+            throw(ArgumentError("$var already exists"))
+        end
+    end
+    df._end = fill(missing,nrow(df))
+    df._seq = fill(missing,nrow(df))
+    df._spell = fill(missing,nrow(df))
+
+    return nothing
+end
+
+function lead!(df,PID::Symbol,TID::Symbol,var::Symbol)
+
+    @warn "currently takes lag. Need to allow `lag()` to take a negative gap input"
+    df[!,"F"*String(var)] = lag(df,PID,TID,var)
+    return nothing
+end
+
+function lag(df,PID::Symbol,TID::Symbol,var::Symbol)
+    df[!,"L"*String(var)] = fill(missing,nrow(df))
+end
+
+function lag!(df,PID::Symbol,TID::Symbol,var::Symbol)
+
+    df[!,"L"*String(var)] = lag(df,PID,TID,var)
+    return nothing
+end
+
+export spell!, lead!, lag!
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,46 @@
 using PanelDataTools
 using Test
+using DataFrames
 
 @testset "PanelDataTools.jl" begin
     # Write your tests here.
+end
+
+function test_df_simple()
+    # Helper function that re-creates this Stata code:
+    #=
+    input id t a
+    1 1 0
+    1 2 0
+    1 3 1
+    2 1 1
+    2 2 1
+    2 3 0
+    end
+    =#
+    df = DataFrame(id = [1,1,1,2,2,2], t = [1,2,3,1,2,3], a = [0,0,1,1,1,0])
+end
+
+@testset "Basic spell!" begin
+    df = test_df_simple()
+    spell!(df,:id,:t,:a)
+
+    @test df.a == [0,0,1,1,1,0]
+    @test df._spell == [1, 1, 2, 1, 1, 2]
+    @test df._seq == [1, 2, 1, 1, 2, 1]
+    @test df._end == [0, 1, 1, 0, 1, 1]
+
+    @test_throws ArgumentError spell!(df,:id,:t,:a)
+end
+
+@testset "Basic lag!" begin
+    df = test_df_simple()
+    lag!(df,:id,:t,:a)
+    @test df.Fa = [missing, 0, 0, missing, 1, 1]
+end
+
+@testset "Basic lead!" begin
+    df = test_df_simple()
+    lead!(df,:id,:t,:a)
+    @test df.La = [0, 1, missing, 1, 1, missing]
 end
