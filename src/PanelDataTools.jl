28d47c6dbfaf68dd47147dd44eeca0c3c6b9357f
Eirik Eylands Brandsaas <eebrandsaas@gmail.com>
2022-07-14 14:08:56 -0400

    Added working spell!

--- src/PanelDataTools.jl
+++ src/PanelDataTools.jl
@@ -4,15 +4,49 @@ module PanelDataTools
 using DataFrames
 using ShiftedArrays
 
+function _assert_panel(df)
+    # This function should have some assertions that check that the panel is balanced etc
+end
+
 function spell!(df,PID::Symbol,TID::Symbol,var::Symbol)
     for var in ["_end", "_seq", "_spell"]
         if "$var" ∈ Set(names(df))
             throw(ArgumentError("$var already exists"))
         end
     end
-    df._end = fill(0,nrow(df))
+
+    _assert_panel(df)
+    gdf = groupby(df,PID)
+    T = nrow(gdf[1])
+
+    strvar = String(var) # String of variable name
+    Lvar = Symbol("L"*strvar)
+    Fvar = Symbol("F"*strvar)
+    lag!(df,PID,TID,var)
+    lead!(df,PID,TID,var)
+    Ltmp = (df[!,var] .== df[!,Lvar])
+    Ftmp = (df[!,var] .== df[!,Fvar])
+
+    df._spell = fill(1,nrow(df))
     df._seq = fill(1,nrow(df))
-    df._spell = fill(2,nrow(df))
+    df._end = fill(false,nrow(df))
+
+    for i = collect(1:nrow(df))[Not(1:T:end)] # All rows except first one for each id
+        if Ltmp[i] == true
+            df[i,:_spell] = df[i-1,:_spell]
+            df[i,:_seq] = df[i-1,:_seq]+1
+        else
+            df[i,:_spell] = df[i-1,:_spell] + 1
+            df[i,:_seq] = 1
+        end
+
+        if isequal(Ftmp[i],false)
+            df[i,:_end] = true
+        end
+    end
+    df[T:T:end,:_end] .= true
+    # for i = collect(1:nrow(df))[Not(T:T:end)] # All rows except first one for each id
+    # end
 
     return nothing
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -38,9 +38,9 @@ end
     spell!(df,:id,:t,:a)
 
     @test df.a == [0,0,1,1,1,0]
-    @test_broken df._spell == [1, 1, 2, 1, 1, 2]
-    @test_broken df._seq == [1, 2, 1, 1, 2, 1]
-    @test_broken df._end == [0, 1, 1, 0, 1, 1]
+    @test df._spell == [1, 1, 2, 1, 1, 2]
+    @test df._seq == [1, 2, 1, 1, 2, 1]
+    @test df._end == [false, true, true, false, true, true]
 
-    @test_throws ArgumentError spell!(df,:id,:t,:a)
+    @test_throws ArgumentError spell!(df,:id,:t,:a) # Check that test throws if variables exist
 end
