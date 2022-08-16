955c937547bd021277abe696468d65b3a6b6895b
Eirik Eylands Brandsaas <eebrandsaas@gmail.com>
2022-08-15 22:12:11 -0400

    Improve tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -49,77 +49,78 @@ function df_misst() # Checking whether package works with different T within pan
     df = DataFrame(id = [1,1,1,2,2,2], t = [1,missing,3,1,2,3], a = [1,1,1,1,0,0])
 end
 
+## Start of tests
+@testset "Basic lag/lead tests" verbose = true begin
+    @testset "Basic lag!" begin
+        df = test_df_simple1()
+        lag!(df,:id,:t,:a)
+        @test isequal(df.L1a,[missing, 0, 0, missing, 1, 1])
 
+        lag!(df,:id,:t,:a,2)
+        @test isequal(df.L2a,[missing, missing, 0, missing, missing, 1])
 
-@testset "Basic lag!" begin
-    df = test_df_simple1()
-    lag!(df,:id,:t,:a)
-    @test isequal(df.L1a,[missing, 0, 0, missing, 1, 1])
+        df = df_diffT()
+        lag!(df,:id,:t,:a)
+        @test isequal(df.L1a,[missing, 1, missing, 1, 0])
 
-    lag!(df,:id,:t,:a,2)
-    @test isequal(df.L2a,[missing, missing, 0, missing, missing, 1])
+        df = df_missid()
+        lag!(df,:id,:t,:a)
+        @test isequal(df.L1a,[missing, 1, 1, missing, missing, missing])
 
-    df = df_diffT()
-    lag!(df,:id,:t,:a)
-    @test isequal(df.L1a,[missing, 1, missing, 1, 0])
+        df = df_gapT()
+        lag!(df,:id,:t,:a)
+        @test isequal(df.L1a, [missing,missing,missing,1,0])
 
-    df = df_missid()
-    lag!(df,:id,:t,:a)
-    @test isequal(df.L1a,[missing, 1, 1, missing, missing, missing])
-
-    df = df_gapT()
-    lag!(df,:id,:t,:a)
-    @test isequal(df.L1a, [missing,missing,missing,1,0])
-
-    df = df_misst()
-    @test_throws TypeError lag!(df,:id,:t,:a)
-    @test_broken isequal(df.L1a,[missing, 1, 1, missing, missing, missing])
-end
-
-@testset "Basic lead!" begin
-    df = test_df_simple1()
-    lead!(df,:id,:t,:a)
-    @test isequal(df.F1a,[0, 1, missing, 1, 0, missing])
-
-    lead!(df,:id,:t,:a,2)
-    @test isequal(df.F2a,[1, missing, missing, 0, missing, missing])
-end
-
-@testset "Multiple lead/lags" begin
-    ## Test that you get the same numbers
-    dfb = test_df_simple1()
-    lag!(dfb,:id,:t,:a)
-    lag!(dfb,:id,:t,:a,2)
-    lead!(dfb,:id,:t,:a)
-    lead!(dfb,:id,:t,:a,2)
-
-    df = test_df_simple1()
-    lag!(df,:id,:t,:a,[1,2,-1,-2])
-
-    for var in [:L1a :L2a :F1a :F2a]
-        @test isequal(df[!,var],dfb[!,var])
-    end
-    df = test_df_simple1()
-    lead!(df,:id,:t,:a,[1,2,-1,-2])
-
-    for var in [:L1a :L2a :F1a :F2a]
-        @test isequal(df[!,var],dfb[!,var])
+        df = df_misst()
+        @test_throws TypeError lag!(df,:id,:t,:a)
+        @test_broken isequal(df.L1a,[missing, 1, 1, missing, missing, missing])
     end
 
-    ## Test that the "indexing" works as expected
-    dfb = test_df_simple1()
-    lag!(dfb,:id,:t,:a)
-    lag!(dfb,:id,:t,:a,2)
-    lead!(dfb,:id,:t,:a)
-    df = test_df_simple1()
-    lag!(df,:id,:t,:a,[1,2,-1])
-    for var in [:L1a :L2a :F1a]
-        @test isequal(df[!,var],dfb[!,var])
+    @testset "Basic lead!" begin
+        df = test_df_simple1()
+        lead!(df,:id,:t,:a)
+        @test isequal(df.F1a,[0, 1, missing, 1, 0, missing])
+
+        lead!(df,:id,:t,:a,2)
+        @test isequal(df.F2a,[1, missing, missing, 0, missing, missing])
     end
-    df = test_df_simple1()
-    lead!(df,:id,:t,:a,[-1,-2,1])
-    for var in [:L1a :L2a :F1a]
-        @test isequal(df[!,var],df[!,var])
+
+    @testset "Multiple lead/lags" begin
+        ## Test that you get the same numbers
+        dfb = test_df_simple1()
+        lag!(dfb,:id,:t,:a)
+        lag!(dfb,:id,:t,:a,2)
+        lead!(dfb,:id,:t,:a)
+        lead!(dfb,:id,:t,:a,2)
+
+        df = test_df_simple1()
+        lag!(df,:id,:t,:a,[1,2,-1,-2])
+
+        for var in [:L1a :L2a :F1a :F2a]
+            @test isequal(df[!,var],dfb[!,var])
+        end
+        df = test_df_simple1()
+        lead!(df,:id,:t,:a,[1,2,-1,-2])
+
+        for var in [:L1a :L2a :F1a :F2a]
+            @test isequal(df[!,var],dfb[!,var])
+        end
+
+        ## Test that the "indexing" works as expected
+        dfb = test_df_simple1()
+        lag!(dfb,:id,:t,:a)
+        lag!(dfb,:id,:t,:a,2)
+        lead!(dfb,:id,:t,:a)
+        df = test_df_simple1()
+        lag!(df,:id,:t,:a,[1,2,-1])
+        for var in [:L1a :L2a :F1a]
+            @test isequal(df[!,var],dfb[!,var])
+        end
+        df = test_df_simple1()
+        lead!(df,:id,:t,:a,[-1,-2,1])
+        for var in [:L1a :L2a :F1a]
+            @test isequal(df[!,var],df[!,var])
+        end
     end
 end
 
@@ -149,8 +150,10 @@ end
 
 
 ## Small test for github issues
-@testset "Issue #4 - had wrong column name in spell!() " begin
-    df = DataFrame(i=[1,1,2,2],year=[1,2,1,2],val=[1,1,1,1])
-    spell!(df,:i,:year,:val)
-    @test df._spell == [1, 1, 1, 1]
+@testset "Issue tests" verbose = true begin
+    @testset "Issue #4 - had wrong column name in spell!() " begin
+        df = DataFrame(i=[1,1,2,2],year=[1,2,1,2],val=[1,1,1,1])
+        spell!(df,:i,:year,:val)
+        @test df._spell == [1, 1, 1, 1]
+    end
 end
